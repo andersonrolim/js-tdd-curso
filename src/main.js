e6a4b00d9f29dae29667771d9742da06e8a1cf85
Renato Assis <renato.assis@prodap.com.br>
2018-03-12 09:21:29 -0300

    fix travis

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const b = 2;
 const c = 2;
 const d = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-console.log(b);
-console.log(c);
-console.log(d);
+
+function sum() {
+  return b + c + d;
+}
+sum();
